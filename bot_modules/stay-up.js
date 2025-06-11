bab1c9ac61a0f504ea085b5098339c546448c3ab
LigitPizza <laeffy7403@gmail.com>
2025-06-12 00:02:23 +0800

    duplicate fixed

--- bot_modules/stay-up.js
+++ bot_modules/stay-up.js
@@ -16,33 +16,12 @@ client.once('ready', () => {
     if (channel) {
       i++;
       channel.send(`${br} stay up every 5 min count: **${i}** 😤`);
-      channel.send(`wes`);
     } else {
       console.log('Channel not found.');
     }
   }, interval);
 });
 
-client.once('ready', () => {
-  console.log(`Logged in as ${client.user.tag}`);
-  const channelId = '1380085584394981428'; // change this
-  // const interval = 10 * 60 * 1000; // 10 minutes in milliseconds
-  const interval = 3 * 60 * 1000; // 3 minutes in milliseconds
-  // const interval = 10* 1000; // 5 sec in milliseconds
-  var i = 0;
-  var br = '--------------------------------------------------------------';
-  
-  setInterval(() => {
-    const channel = client.channels.cache.get(channelId);
-    if (channel) {
-      i++;
-      // channel.send(`${br} stay up every 5 min count: **${i}** 😤`);
-      channel.send(`wes`);
-    } else {
-      console.log('Channel not found.');
-    }
-  }, interval);
-});
 
 client.once('ready', () => {
   console.log(`Logged in as ${client.user.tag}`);
@@ -73,6 +52,7 @@ client.once('ready', () => {
     }
   }, interval);
 });
+
 
 
 client.on("messageCreate", message => {
