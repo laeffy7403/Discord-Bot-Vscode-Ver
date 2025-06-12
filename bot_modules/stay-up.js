7bff92feb9f87afe618e877c5b5e682bb5149afc
LigitPizza <laeffy7403@gmail.com>
2025-06-13 01:15:00 +0800

    switch var to conastant

--- bot_modules/stay-up.js
+++ bot_modules/stay-up.js
@@ -4,11 +4,14 @@ const client = new Discord.Client({
   intents: ["GUILDS", "GUILD_MESSAGES"]
 });
 
+const UP_TIME_1 = 10; 
+// const UP_TIME_2 = 10;
+const BR = '----------------------------------------';
 
 client.once('ready', () => {
   console.log(`Logged in as ${client.user.tag} 6 min up`);
   const channelId = '1380085584394981428'; // change this
-  const interval = 10 * 60 * 1000; // 10 minutes in milliseconds
+  const interval = UP_TIME_1 * 60 * 1000; // 10 minutes in milliseconds
   var i = 0;
   var br = '--------------------------------------------------------------';
   
@@ -16,7 +19,7 @@ client.once('ready', () => {
     const channel = client.channels.cache.get(channelId);
     if (channel) {
       i++;
-      channel.send(`${br} stay up every 10 min count: **${i}** 😤`);
+      channel.send(`${BR} stay up every ${UP_TIME_1} min count: **${i}** 😤`);
       // channel.send(`monika wes -`);
     } else {
       console.log('Channel not found.');
@@ -29,13 +32,12 @@ client.once('ready', () => {
   const channelId = '1380085584394981428'; // change this
   const interval = 30 * 1000; // 30 sec in milliseconds
   var i = 0;
-  var br = '----------------------------------------';
   
   setInterval(() => {
     const channel = client.channels.cache.get(channelId);
     if (channel) {
       i++;
-      // channel.send(`${br} wes - 1min counter: **${i}** `);
+      // channel.send(`${BR} wes - 1min counter: **${i}** `);
       channel.send(`<@970610501589544991>`);
     } else {
       console.log('Channel not found.');
@@ -54,7 +56,7 @@ client.once('ready', () => {
 //     const channel = client.channels.cache.get(channelId);
 //     if (channel) {
 //       i++;
-//       channel.send(`${br}monika - 10min counter: **${i}** `);
+//       channel.send(`${BR}monika - 10min counter: **${i}** `);
 //       channel.send(`monika wes -`);
 //     } else {
 //       console.log('Channel not found.');
@@ -64,21 +66,17 @@ client.once('ready', () => {
 
 
 
-client.on("messageCreate", message => {
-  // Only respond in the specific channel
-  if (message.channel.id !== '1380085584394981428') return;
+// client.on("messageCreate", message => {
+//   // Only respond in the specific channel
+//   if (message.channel.id !== '1380085584394981428') return;
   
-  const msgContent = message.content.toLowerCase();
-  var signal = ["wes", "monika", "m", "o", "n", "i", "k", "a", "w", "e", "s", "-"];
+//   const msgContent = message.content.toLowerCase();
+//   var signal = ["wes", "monika", "m", "o", "n", "i", "k", "a", "w", "e", "s", "-"];
   
-  if (signal.some(trigger => msgContent.includes(trigger))) {
-    message.channel.send("monika monika monika monika monika monika!");
-  }
-});
-
-
-
-
+//   if (signal.some(trigger => msgContent.includes(trigger))) {
+//     message.channel.send("monika monika monika monika monika monika!");
+//   }
+// });
 
 
 client.login(process.env.DIS_TOKEN);
